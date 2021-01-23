fe3220ecec4a7d9734d13dee810101401b807c74
MANJUPARKAVI T <manjutv1999@gmail.com>
2021-01-24 00:12:25 +0530

    Updated

--- src/service/CRUDOperations.java
+++ src/service/CRUDOperations.java
@@ -1,29 +1,78 @@
 package service;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 import model.Blog;
 
 public class CRUDOperations {
-	static List<Blog> list=new ArrayList<Blog>(); // Store the blog in the list
-	
-	public List<Blog> createBlog(Blog blog) throws IOException {
+	static List<Blog> list=new ArrayList<Blog>(); // Store the blog in the
+	public List<Blog> addBlog(Blog blog) throws IOException {
 		list.add(blog);
+		System.out.println("After adding an element:");
+		for(Blog item:list) {
+			System.out.println(item);
+		}
 		return list;
 	}
 	
 	public List<Blog> viewBlog(Blog blog) throws IOException{
-		return null;
+		
+		for(Blog item:list) {
+			System.out.println(item);
+		}
+		return list;
 		
 	}
 	public List<Blog> updateBlog(Blog blog) throws IOException{
-		return null;
+		list.set(list.indexOf(blog), blog);
+		return list;
 	
 	}
 	public List<Blog> deleteBlog(Blog blog) throws IOException{
-		return null;
+		list.remove(blog);
+		return list;
 		
 	}
+	public List<Blog> sortByTitle(){
+		Collections.sort(list,(Blog b1,Blog b2)->b1.getBlogTitle().compareTo(b2.getBlogTitle()));
+		System.out.println("After Sorting By Title Using List");
+		for(Blog x:list) {
+			System.out.println(x.getBlogTitle());
+			System.out.println(x.getBlogDescription());
+			System.out.println(x.getDate());
+		}
+
+	
+		TreeSet<Blog> set=new TreeSet<Blog>(list);
+		System.out.println("After Sorting By Title Using Set");
+		for(Blog x:set) {
+			System.out.println(x.getBlogTitle());
+			System.out.println(x.getBlogDescription());
+			System.out.println(x.getDate());
+		
+	    }
+		return list;
+	}
+	public List<Blog> sortByDate(){
+		Collections.sort(list,(b1,b2)->b1.getDate().compareTo(b2.getDate()));
+		System.out.println("After Sorting By Date Using List");
+		for(Blog x:list) {
+			System.out.println(x.getBlogTitle());
+			System.out.println(x.getBlogDescription());
+			System.out.println(x.getDate());
+		}
+
+	
+		TreeSet<Blog> set=new TreeSet<Blog>(list);
+		System.out.println("After Sorting By Date Using Set");
+		for(Blog x:set) {
+			System.out.println(x.getBlogTitle());
+			System.out.println(x.getBlogDescription());
+			System.out.println(x.getDate());
+		
+	    }
+		return list;
+	}
+	
 }
